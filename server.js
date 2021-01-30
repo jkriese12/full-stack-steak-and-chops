cd8cb08077e7b617e8bed025eb09b8a357c1c2d9
Brando D <brando2147@gmail.com>
2021-01-30 10:59:01 -0700

    initial commit

---
+++ server.js
@@ -0,0 +1,53 @@
+
+var express = require("express");
+var path = require("path");
+
+
+var app = express();
+var PORT = 3000;
+
+// Sets up the Express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// (DATA)
+// =============================================================
+var arrData = [
+    {
+
+
+    },
+    {
+
+
+    },
+    {
+
+
+    }
+];
+
+// Routes
+// =============================================================
+
+app.get("/", function (req, res) {
+    res.sendFile(path.join(__dirname, "index.html"));
+});
+
+app.get("/reserve", function (req, res) {
+    res.sendFile(path.join(__dirname, "add.html"));
+});
+
+app.get("/tables", function (req, res) {
+    res.sendFile(path.join(__dirname, "add.html"));
+});
+
+
+
+
+
+// Starts the server to begin listening
+// =============================================================
+app.listen(PORT, function () {
+    console.log("App listening on PORT " + PORT);
+});
